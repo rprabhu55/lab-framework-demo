14c149c5b0d72d749c6d697839682e59dcee3482
Cody Green <cody@codygreen.com>
2024-07-13 16:01:17 -0500

    add code block copy

--- src/app/components/codeblock.js
+++ src/app/components/codeblock.js
@@ -1,24 +1,33 @@
 // components/CodeBlock.js
+
+"use client";
+
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { useState } from 'react';
+import { FiClipboard } from 'react-icons/fi';
 
 export function CodeBlock ({ className, children }) {
   const language = className?.replace('language-', '') || 'javascript';
-  return (
-    <p className="my-4">
-        <SyntaxHighlighter language={language} style={solarizedlight}>
-        {children}
-        </SyntaxHighlighter>
-    </p>
-  );
+  const [copied, setCopied] = useState(false);
 
-// export function CodeBlock ({ props }) {
-//   const language = props.className?.replace('language-', '') || 'javascript';
-//   return (
-//     <div className="my-4">
-//         <SyntaxHighlighter language={language} style={solarizedlight}>
-//         {props.children}
-//         </SyntaxHighlighter>
-//     </div>
-//   );
+  const handleCopy = () => {
+    navigator.clipboard.writeText(children.trim());
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+  return (
+    <div className="relative bg-gray-800 rounded-lg">
+      <button
+        onClick={handleCopy}
+        className="absolute right-2 top-2 bg-gray-700 text-white p-1 rounded-md hover:bg-gray-600 focus:outline-none"
+      >
+        {copied ? 'Copied' : <FiClipboard />}
+      </button>
+      <SyntaxHighlighter language={language} style={solarizedlight}>
+        {children}
+      </SyntaxHighlighter>
+    </div>
+  );
 };
