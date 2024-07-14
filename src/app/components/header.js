d64c63b7fbfcd52c877d38a6613dd3c27096f8f2
Cody Green <cody@codygreen.com>
2024-07-13 23:15:51 -0500

    fix logo

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,9 +5,9 @@ import Image from 'next/image'
 export function Header() {
   return (
     <header className="bg-white shadow">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+      <nav className="mx-auto flex max-w-9xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">  
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Lab Framework</span>
             <Image src="/lab-framework-logo.png" alt="Lab Framework" width={50} height={50} />
           </a>
