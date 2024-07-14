d29f56d1bf7a03cfd83d394004a4fca6cec05725
Cody Green <cody@codygreen.com>
2024-07-13 22:55:33 -0500

    initial API Test example

---
+++ src/app/components/apicheck.js
@@ -0,0 +1,46 @@
+"use client"
+import { useState } from 'react'
+
+export function ApiCheck({name, url}) {
+    const [isOnline, setIsOnline] = useState(null)
+    const CheckWebsiteStatus = async () => {    
+        console.log('check')
+        console.log(url)
+        if (url) {
+            await fetch(url, {cache: "no-store", mode: "no-cors"})
+            // .then(response => response.text())
+            .then(response => response.ok)
+            .then(() => {
+                setIsOnline(true)
+            })
+            .catch(error => {
+                console.error('Error:', error)
+                setIsOnline(false)
+            });
+        }
+    }
+
+    const handleClick = () => {
+        CheckWebsiteStatus(url);
+        console.log(getColor())
+    }
+    
+    const getColor = () => {
+        if (isOnline === null) return 'bg-orange-50'
+        return isOnline ? 'bg-green-200' : 'bg-red-200'
+    }
+
+    return (
+        <div className={"w-1/4 flex justify-center items-center ".concat(getColor())}>
+        <span>
+            <p className="text-gray-700 text-lg font-semibold">{name}</p>
+            <p className="text-center">
+                <button 
+                    className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                    onClick={() => handleClick(url)}
+                >Check</button>
+            </p>
+            </span> 
+        </div>
+    )
+}
--- src/app/lab/page.js
+++ src/app/lab/page.js
@@ -1,14 +1,15 @@
 import fs from "fs"
 import path from "path"
 import { MDXRemote } from 'next-mdx-remote/rsc'
-// import lab from "./lab.mdx"
+import { ApiCheck } from "@/app/components/apicheck"
 
 const contentDir = path.join(process.cwd(), "src/app/lab")
 const source = path.join(contentDir,"./lab.mdx")
+const components = { ApiCheck }
 
 export default function Page() {
   return <MDXRemote 
-    source={fs.readFileSync(source, "utf8")}
-    // source={lab}
+    source={fs.readFileSync(source, "utf8")} 
+    components={components}
   />
 }
