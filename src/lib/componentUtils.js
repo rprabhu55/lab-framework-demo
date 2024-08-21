124251ec48c5db964259a34be959ed2b5b423a95
Daniel Edgar <aknot242@gmail.com>
2024-08-21 01:57:48 +0000

    update apicheck to server side

---
+++ src/lib/componentUtils.js
@@ -0,0 +1,16 @@
+"use server"
+export async function checkWebsiteStatus(url) {
+    // console.log("check")
+    // console.log(url)
+    if (url) {
+        await fetch(url, { cache: "no-store", mode: "no-cors" })
+            .then(response => response.ok)
+            .then(() => {
+                return true
+            })
+            .catch(error => {
+                console.error("Error:", error)
+                return false
+            })
+    }
+}
