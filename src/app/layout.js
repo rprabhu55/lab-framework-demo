acb04961fa1621ff881e4f146fe78cf5e842c8ae
Daniel Edgar <aknot242@gmail.com>
2024-08-23 14:28:44 +0000

    introduce linting

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import { Header } from "@/app/components/header";
 import "./globals.css";
 
+export const dynamic = 'force-dynamic'
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
