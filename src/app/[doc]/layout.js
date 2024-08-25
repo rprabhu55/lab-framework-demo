53146b83b59ba59282a5dacda1ed608949322d07
Cody Green <cody@codygreen.com>
2024-08-25 04:21:33 +0000

    Table of contents display hierarchy Fixes #105

--- src/app/[doc]/layout.js
+++ src/app/[doc]/layout.js
@@ -4,7 +4,7 @@ export default function MdxLayout({ children }) {
   
   return (
     <div className="flex">
-      <div className="mt-0 pt-0 pl-2 pr-0 bg-gray-100">
+      <div className="mt-0 pt-0 pl-2 pr-0 bg-white">
         <ToC />
       </div>
       <div className="flex-grow p-4 md:pr-10 sm:pr-4 md:flex-wrap md:justify-center">
--- src/app/components/toc.js
+++ src/app/components/toc.js
@@ -7,10 +7,11 @@ export function ToC() {
 
   useEffect(() => {
     // Get all the headings on the page
-    const headingElements = document.querySelectorAll("h1, h2")
+    const headingElements = document.querySelectorAll("h1, h2, h3, h4")
     const headings = Array.from(headingElements).map((heading) => ({
       title: heading.innerText,
       id: heading.innerText.toLowerCase().replace(/\s/g, "-"),
+      level: parseInt(heading.tagName.substring(1)), // Get the heading level
     }))
     setHeadings(headings)
   }, [])
@@ -19,9 +20,12 @@ export function ToC() {
     <div className="toc-container p-1 w-80 sticky top-0 border-r-2 border-t-2 h-screen">
       <ul className="list-none">
         {headings.map((heading) => (
-          <li key={heading.id} className="">
-            <a className="no-underline text-gray-500 text-base" href={`#${heading.id}`}>
-              {heading.title.substring(0,)}
+          <li key={heading.id} className="" style={{ 
+            fontSize: `${1.5 - (heading.level * 0.2)}em`, // Example: h1 -> 1.3em, h2 -> 1.1em, etc.
+            marginLeft: `${(heading.level - 1) * 20}px` // Example: h1 -> 0px, h2 -> 20px, etc.
+          }}>
+            <a className="no-underline text-gray-600 text-base" href={`#${heading.id}`}>
+              {heading.title}
             </a>
           </li>
         ))}
