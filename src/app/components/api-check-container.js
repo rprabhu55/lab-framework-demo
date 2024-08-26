2b6fe3f50ca7ab85bead1d18bb4dd5160f055d23
Daniel Edgar <aknot242@gmail.com>
2024-08-26 00:58:44 +0000

    fix import, add author code

--- src/app/components/api-check-container.js
+++ src/app/components/api-check-container.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { ApiCheck } from "@/app/components/api-check";
+import { APICheck } from "@/app/components/api-check";
 
 /**
- * ApiCheckContainer component
+ * APICheckContainer component
  * 
- * This component filters and renders only the children that are of type ApiCheck.
- * It wraps each ApiCheck component in a div with a specific class for styling.
+ * This component filters and renders only the children that are of type APICheck.
+ * It wraps each APICheck component in a div with a specific class for styling.
  * 
  * @param {Object} props - The properties object.
  * @param {React.ReactNode} props.children - The child components to be filtered and rendered.
  * 
- * @returns {JSX.Element} The rendered ApiCheck component.
+ * @returns {JSX.Element} The rendered APICheck component.
  * 
  * @example
- * <ApiCheckContainer>
- *   <ApiCheck id="apicheck1" />
- *   <ApiCheck id="apicheck2" />
- *   <div>Not an ApiCheck component</div>
- * </ApiCheckContainer>
+ * <APICheckContainer>
+ *   <APICheck id="apicheck1" />
+ *   <APICheck id="apicheck2" />
+ *   <div>Not an APICheck component</div>
+ * </APICheckContainer>
  */
-const ApiCheckContainer = ({ children }) => {
-  const apiCheckComponents = React.Children.toArray(children).filter(child => child.type === ApiCheck);
+const APICheckContainer = ({ children }) => {
+  const apiCheckComponents = React.Children.toArray(children).filter(child => child.type === APICheck);
 
   return (
     <div id="api-check-outer-container" className="flex flex-col border border-gray-300 p-4 rounded">
-      <span className="font-bold text-xl">ApiCheck Containers</span>
+      <span className="font-bold text-xl">API Checks</span>
       <div id="api-check-inner-container" className="flex flex-wrap gap-4">
         {apiCheckComponents.map((component, index) => (
           <div key={index} className="api-check-item">
@@ -36,4 +36,4 @@ const ApiCheckContainer = ({ children }) => {
   );
 };
 
-export default ApiCheckContainer;
+export default APICheckContainer;
--- src/lib/mdxComponents.js
+++ src/lib/mdxComponents.js
@@ -1,5 +1,4 @@
-import { APICheck } from "@/app/components/api-check"
-import { APICheckContainer } from "@/app/components/api-check-container"
+import { APICheck } from "@/app/components/api-check";
 import { APIHeaderCheck } from "@/app/components/api-header-check";
 import { APIResponseCheck } from "@/app/components/api-response-check";
 import { CodeBlock } from "@/app/components/codeblock";
@@ -9,24 +8,25 @@ import { GetVariable } from "@/app/components/get-variable";
 import { InputVariable } from "@/app/components/input-var";
 import { UdfDeploymentMetadata } from "@/app/components/udf-deployment-metadata";
 import DockerContainer from "@/app/components/docker-container";
+import APICheckContainer from "@/app/components/api-check-container";
 
 const MDXComponents = {
-    APICheck,
-    APICheckContainer,
-    APIHeaderCheck,
-    APIResponseCheck,
-    CodeBlock,
-    Docker,
-    DockerStatus,
-    DockerContainer,
-    GetVariable,
-    InputVariable,
-    UdfDeploymentMetadata,
-    h1: (props) => <h1 id={props.children.toLowerCase().replace(/\s/g, "-")} {...props} />,
-    h2: (props) => <h2 id={props.children.toLowerCase().replace(/\s/g, "-")} {...props} />,
-    h3: (props) => <h3 id={props.children.toLowerCase().replace(/\s/g, "-")} {...props} />,
-    h4: (props) => <h4 id={props.children.toLowerCase().replace(/\s/g, "-")} {...props} />,
-    code: (props) => <CodeBlock {...props} />
-  }
+  APICheck,
+  APICheckContainer,
+  APIHeaderCheck,
+  APIResponseCheck,
+  CodeBlock,
+  Docker,
+  DockerStatus,
+  DockerContainer,
+  GetVariable,
+  InputVariable,
+  UdfDeploymentMetadata,
+  h1: (props) => <h1 id={props.children.toLowerCase().replace(/\s/g, "-")} {...props} />,
+  h2: (props) => <h2 id={props.children.toLowerCase().replace(/\s/g, "-")} {...props} />,
+  h3: (props) => <h3 id={props.children.toLowerCase().replace(/\s/g, "-")} {...props} />,
+  h4: (props) => <h4 id={props.children.toLowerCase().replace(/\s/g, "-")} {...props} />,
+  code: (props) => <CodeBlock {...props} />
+}
 
-  export default MDXComponents
+export default MDXComponents
