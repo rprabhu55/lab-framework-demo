2eb8e89ae819c63058cdaeb0b195101b7abe3086
Daniel Edgar <aknot242@gmail.com>
2024-08-26 12:04:39 +0000

    fix: add alt text to images

--- src/app/components/image-modal-client.js
+++ src/app/components/image-modal-client.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 
-const ImageModalClient = ({ src }) => {
+const ImageModalClient = ({ src, alt }) => {
   const [modalSrc, setModalSrc] = useState(null);
 
   const handleImageClick = () => {
@@ -31,12 +31,12 @@ const ImageModalClient = ({ src }) => {
         src={src}
         className="cursor-pointer"
         onClick={handleImageClick}
-        alt="Thumbnail"
+        alt={`${alt} (thumbnail)`}
       />
       {modalSrc && (
         <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
           <div className="relative">
-            <img src={modalSrc} alt="Full Size" className="max-w-full max-h-full" />
+            <img src={modalSrc} alt={`${alt} (full size)`} className="max-w-full max-h-full" />
             <button
               onClick={closeModal}
               className="absolute top-0 right-0 m-4 text-white text-2xl"
