c97b6e8631ec2fc687fd6768c1cb71ee89d3e6cd
Daniel Edgar <aknot242@gmail.com>
2024-08-26 01:26:37 +0000

    include support for all 3 check components

--- src/app/components/api-check-container.js
+++ src/app/components/api-check-container.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { APICheck } from "@/app/components/api-check";
+import { APIHeaderCheck } from "@/app/components/api-header-check";
+import { APIResponseCheck } from "@/app/components/api-response-check";
 
 /**
  * APICheckContainer component
  * 
- * This component filters and renders only the children that are of type APICheck.
+ * This component filters and renders only the children that are of type APICheck, APIHeaderCheck or APIResponseCheck.
  * It wraps each APICheck component in a div with a specific class for styling.
  * 
  * @param {Object} props - The properties object.
@@ -15,17 +17,19 @@ import { APICheck } from "@/app/components/api-check";
  * @example
  * <APICheckContainer>
  *   <APICheck id="apicheck1" />
- *   <APICheck id="apicheck2" />
+ *   <APIHeaderCheck id="apicheck2" />
+ *   <APIResponseCheck id="apicheck3" />
  *   <div>Not an APICheck component</div>
  * </APICheckContainer>
  */
 const APICheckContainer = ({ children }) => {
-  const apiCheckComponents = React.Children.toArray(children).filter(child => child.type === APICheck);
+  const apiCheckComponents = React.Children.toArray(children).filter(
+    child => [APICheck, APIHeaderCheck, APIResponseCheck].includes(child.type));
 
   return (
     <div id="api-check-outer-container" className="flex flex-col border border-gray-300 p-4 rounded">
-      <span className="font-bold text-xl">API Checks</span>
-      <div id="api-check-inner-container" className="flex flex-wrap gap-4">
+      <span className="font-bold text-xl">Checks</span>
+      <div id="api-check-inner-container" className="flex flex-wrap gap-4 mt-3">
         {apiCheckComponents.map((component, index) => (
           <div key={index} className="api-check-item">
             {component}
