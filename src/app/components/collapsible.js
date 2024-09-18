1b063c6c5df0dc2ebc08c4c95037ea01ea0b9035
John Wong <john.wong@f5.com>
2024-09-18 06:24:15 +0000

    Updating collapsible component

--- src/app/components/collapsible.js
+++ src/app/components/collapsible.js
@@ -1,35 +1,14 @@
 // components/Collapsible.js
 "use client";
 
-// import { useState } from 'react';
-import {Accordion, AccordionItem} from "@nextui-org/react";
+import {Accordion, AccordionItem} from "@nextui-org/accordion";
 
-export function Collapsible ({ title, children }) {
-  // const [isOpen, setIsOpen] = useState(false);
-
-  // const toggleOpen = () => {
-  //   setIsOpen(!isOpen);
-  // };
-
-  // return (
-  //   <div>
-  //     <button onClick={toggleOpen} style={{ backgroundColor: 'lightgray', padding: '10px', border: 'none', cursor: 'pointer' }}>
-  //       {title}
-  //     </button>
-  //     {isOpen && (
-  //       <div style={{ marginTop: '10px', padding: '10px', border: '1px solid lightgray' }}>
-  //         {children}
-  //       </div>
-  //     )}
-  //   </div>
-  // );
-
+export function Collapsible ({ title, header, children }) {
   return (
     <Accordion selectionMode="multiple">
-      <AccordionItem style={{ backgroundColor: 'lightgray'}} key="1" aria-label="Accordion 1" title={title}>
-      {children}
+      <AccordionItem style={{ backgroundColor: 'lightgray'}} title={title} HeadingComponent={header}>
+        {children}
       </AccordionItem>
     </Accordion>
   );
-
 }
